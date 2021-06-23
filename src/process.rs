0c70ad7d52a9af035e3aa1c0c740904fd6a802e2
Sanskar Jethi <sansyrox@gmail.com>
2021-06-24 00:32:50 +0530

    Code cleanup using PyFunction type

--- src/process.rs
+++ src/process.rs
@@ -1,46 +1,32 @@
 use tokio::io::AsyncWriteExt;
 use tokio::net::TcpStream;
 // pyO3 module
+use crate::types::PyFunction;
 use pyo3::prelude::*;
-use pyo3::types::{PyAny, PyDict};
 
-enum PyFunction {
-    CoRoutine(Py<PyAny>),
-    SyncFunction(Py<PyAny>),
-}
+// Handle message fetches the response function
+// function is the response function fetched from the router
+// tokio task is spawned depending on the type of function fetched (Sync/Async)
 
-pub async fn handle_message(process_object: Py<PyAny>, mut stream: TcpStream) {
-    let function: PyFunction = Python::with_gil(|py| {
-        let process_object_wrapper: &PyAny = process_object.as_ref(py);
-        let py_dict = process_object_wrapper.downcast::<PyDict>().unwrap();
-        let is_async: bool = py_dict.get_item("is_async").unwrap().extract().unwrap();
-        let handler: &PyAny = py_dict.get_item("handler").unwrap();
-        if is_async {
-            let coro = handler.call0().unwrap();
-            PyFunction::CoRoutine(coro.into())
-        } else {
-            PyFunction::SyncFunction(handler.into())
-        }
-    });
-
+pub async fn handle_message(function: PyFunction, mut stream: TcpStream) {
     let contents = match function {
-        PyFunction::CoRoutine(coro) => {
-            let x = Python::with_gil(|py| {
-                let x = coro.as_ref(py);
-                pyo3_asyncio::into_future(x).unwrap()
+        PyFunction::CoRoutine(handler) => {
+            let output = Python::with_gil(|py| {
+                let coro = handler.as_ref(py).call0().unwrap();
+                pyo3_asyncio::into_future(coro).unwrap()
             });
-            let output = x.await.unwrap();
+            let output = output.await.unwrap();
             Python::with_gil(|py| -> PyResult<String> {
                 let contents: &str = output.extract(py).unwrap();
                 Ok(contents.to_string())
             })
             .unwrap()
         }
-        PyFunction::SyncFunction(x) => tokio::task::spawn_blocking(move || {
+        PyFunction::SyncFunction(handler) => tokio::task::spawn_blocking(move || {
             Python::with_gil(|py| {
-                let y = x.as_ref(py);
-                let s: &str = (&y).call0().unwrap().extract().unwrap();
-                String::from(s)
+                let handler = handler.as_ref(py);
+                let output: &str = (&handler).call0().unwrap().extract().unwrap();
+                output.to_string()
             })
         })
         .await
