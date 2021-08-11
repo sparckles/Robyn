08dbb1414493333c2f7b23f8150a22d0f94e7972
Sanskar Jethi <sansyrox@gmail.com>
2021-08-11 13:00:02 +0530

    Add the static directory serving (#64)
    * Add the static directory serving
    
    * Add rw lock for static directory serving

--- src/lib.rs
+++ src/lib.rs
@@ -9,18 +9,9 @@ use server::Server;
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
-#[pyfunction]
-pub fn start_server() {
-    // this is a wrapper function for python
-    // to start a server
-    Server::new();
-}
-
 #[pymodule]
 pub fn robyn(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     // the pymodule class to make the rustPyFunctions available
-    // in python
-    m.add_wrapped(wrap_pyfunction!(start_server))?;
     m.add_class::<Server>()?;
     pyo3::prepare_freethreaded_python();
     Ok(())
