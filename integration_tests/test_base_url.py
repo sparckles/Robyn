a004dc3bc330155d1bd269a9c689b36d67446832
Sanskar Jethi <sansyrox@gmail.com>
2022-01-05 17:44:58 +0530

    Add tests

--- integration_tests/test_base_url.py
+++ integration_tests/test_base_url.py
@@ -6,7 +6,14 @@ def test_local_index_request(session):
     res = requests.get(f"{BASE_URL}")
     assert(res.status_code == 200)
 
+
 def test_global_index_request(global_session):
+    BASE_URL = "http://0.0.0.0:5000"
+    res = requests.get(f"{BASE_URL}")
+    assert(res.status_code == 200)
+
+
+def test_dev_index_request(dev_session):
     BASE_URL = "http://0.0.0.0:5000"
     res = requests.get(f"{BASE_URL}")
     assert(res.status_code == 200)
