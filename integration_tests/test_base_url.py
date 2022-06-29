0c572a9ee1f846301afa1313a79ed5778a449f36
Sanskar Jethi <sansyrox@gmail.com>
2022-06-29 20:33:08 +0100

    test(add-scope-in-tests): fix the need for the freeport dependency and add scopes

--- integration_tests/test_base_url.py
+++ integration_tests/test_base_url.py
@@ -1,4 +1,5 @@
 import requests
+import os
 
 
 def test_default_url_index_request(default_session):
@@ -10,17 +11,20 @@ def test_default_url_index_request(default_session):
 def test_local_index_request(session):
     BASE_URL = "http://127.0.0.1:5000"
     res = requests.get(f"{BASE_URL}")
+    assert(os.getenv("ROBYN_URL") == "127.0.0.1")
     assert(res.status_code == 200)
 
 
 def test_global_index_request(global_session):
     BASE_URL = "http://0.0.0.0:5000"
     res = requests.get(f"{BASE_URL}")
+    assert(os.getenv("ROBYN_URL") == "0.0.0.0")
     assert(res.status_code == 200)
 
 
 def test_dev_index_request(dev_session):
-    BASE_URL = "http://0.0.0.0:5000"
+    BASE_URL = "http://127.0.0.1:5001"
     res = requests.get(f"{BASE_URL}")
+    assert(os.getenv("ROBYN_PORT") == "5001")
     assert(res.status_code == 200)
 
