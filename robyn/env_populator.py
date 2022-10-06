1c2e30e0179fafc3214386187fefcf48ab526959
Shending-Help <yusufcheetos99@gmail.com>
2022-10-07 00:42:36 +0200

    testing env support

--- robyn/env_populator.py
+++ robyn/env_populator.py
@@ -29,10 +29,8 @@ def parser(config_path=CONFIG_PATH):
 def load_vars(variables = None):
     """Main function"""
     
-    variables = parser() 
-
     if variables is None:
-        return
+        variables = parser()
 
     for var in variables:
         if var[0] in os.environ:
