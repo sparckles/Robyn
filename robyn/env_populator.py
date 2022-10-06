549723a89c0845472b58acf2039ba2af22c58ef2
Youssef Sameh shokry <43523841+Shending-Help@users.noreply.github.com>
2022-10-07 00:06:06 +0200

    feature: add support for env variables (#286)
    * define root ar definitions.py
    - parse and set in import_vars
    
    * file detector, parser and setting vars
    
    * Delete hello_robyn.py
    
    * Delete configuration.conf
    
    * name, functions are now testable, var overrides?
    
    * testing files
    
    * pathlib instead of os, moving consts, logging
    
    * calling the load_vars() automatically, checking for robyn.env first
    
    * fixing the load_vars() call
    
    * calling the load_vars() function in the constructor

---
+++ robyn/env_populator.py
@@ -0,0 +1,48 @@
+import os 
+import logging
+from pathlib import Path
+
+
+
+# Path to the root of the project
+ROOT_DIR = Path(__file__).parent.parent
+
+# Path to the environment variables
+CONFIG_PATH = ROOT_DIR / 'robyn.env'
+
+#set the logger that will log the environment variables imported from robyn.env and the ones already set
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+# parse the configuration file returning a list of tuples (key, value) containing the environment variables
+def parser(config_path=CONFIG_PATH):
+    """Parse the configuration file"""
+    if config_path.exists():
+        with open(config_path, 'r') as f:
+            for line in f:
+                if line.startswith('#'):
+                    continue
+                yield line.strip().split('=')  
+
+
+# check for the environment variables set in cli and if not set them
+def load_vars(variables = None):
+    """Main function"""
+    
+    variables = parser() 
+
+    if variables is None:
+        return
+
+    for var in variables:
+        if var[0] in os.environ:
+            logger.info(f" Variable {var[0]} already set")
+            continue
+        else:
+            os.environ[var[0]]=var[1]   
+            logger.info(f" Variable {var[0]} set to {var[1]}")
+            
+
+    
+
+
