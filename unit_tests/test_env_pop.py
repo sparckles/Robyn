90e4221a5325e398c1e7c647c6c4350c734c601b
Shending-Help <yusufcheetos99@gmail.com>
2022-10-07 17:32:00 +0200

    moving unit tests to a seperate folder, reseting variables set during testing

--- unit_tests/test_env_pop.py
+++ unit_tests/test_env_pop.py
@@ -1,9 +1,8 @@
-from integration_tests.conftest import test_session
 from robyn.env_populator import load_vars, parser
 import pathlib
 import os
 import pytest
-import requests
+
 
 
 
@@ -20,12 +19,13 @@ def env_file():
     env_file.unlink()
     os.unsetenv("PORT")
 
-# this tests if a connection can be made to the server with the correct port imported from the env file
-def test_env_population(test_session, env_file):
+def test_parser(env_file):
+    dir = path / "test_dir"
+    env_file = dir / "robyn.env"
+    assert list(parser(config_path = env_file)) == [['PORT', '8080']]
+
+def test_load_vars(env_file):
     dir = path / "test_dir"
     env_file = dir / "robyn.env"
     load_vars(variables = parser(config_path = env_file))
-    port = os.environ['PORT'] 
-    BASE_URL = f"http://127.0.0.1:{port}"
-    res = requests.get(f"{BASE_URL}")
-    assert res.status_code == 200
+    assert os.environ['PORT'] == '8080'
