eae0480fe06f610e73fa79e157ebe3b1777bf6e6
Sanskar Jethi <sansyrox@gmail.com>
2022-11-06 09:21:00 +0000

    fix: log level not working (#303)
    * fix: remove extra log level config
    
    * style: reformat integration_tests/conftest.py

--- integration_tests/conftest.py
+++ integration_tests/conftest.py
@@ -7,26 +7,26 @@ import pathlib
 import os
 import time
 
+
 def spawn_process(command: List[str]) -> subprocess.Popen:
     if sys.platform.startswith("win32"):
         command[0] = "python"
-        process = subprocess.Popen(command, shell=True, creationflags=subprocess.CREATE_NEW_PROCESS_GROUP)
+        process = subprocess.Popen(
+            command, shell=True, creationflags=subprocess.CREATE_NEW_PROCESS_GROUP
+        )
         return process
     process = subprocess.Popen(command, preexec_fn=os.setsid)
-    return process 
-    
+    return process
 
 
 def kill_process(process: subprocess.Popen) -> None:
     if sys.platform.startswith("win32"):
         process.send_signal(signal.CTRL_BREAK_EVENT)
         process.kill()
-        return     
+        return
     os.killpg(os.getpgid(process.pid), signal.SIGKILL)
-    
-        
-       
-        
+
+
 @pytest.fixture(scope="session")
 def session():
     os.environ["ROBYN_URL"] = "127.0.0.1"
@@ -37,8 +37,6 @@ def session():
     time.sleep(5)
     yield
     kill_process(process)
-    
-
 
 
 @pytest.fixture(scope="session")
@@ -76,6 +74,7 @@ def dev_session():
     yield
     kill_process(process)
 
+
 @pytest.fixture(scope="session")
 def test_session():
     os.environ["ROBYN_URL"] = "127.0.0.1"
@@ -87,4 +86,3 @@ def test_session():
     time.sleep(5)
     yield
     kill_process(process)
-
