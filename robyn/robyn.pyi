241d7faf13a6fcb7eaecd83751a21446467269be
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-04-07 01:30:47 -0400

    Rename params with path_params (#460)
    * Change params by path_params
    
    * Change `params` for `path_params` in params' tests
    
    * Change `params` for `path_params`
    
    * Change `params` for `path_params`
    
    * Update pyproject.toml
    
    * Update Cargo.toml
    
    * Release v0.26.1
    
    * Fix the version of Robyn
    
    * Fix formatting
    
    * Fix parameter of `sync_param` function
    
    * Update pyproject.toml
    
    ---------
    
    Co-authored-by: Sanskar Jethi <sansyrox@gmail.com>

--- robyn/robyn.pyi
+++ robyn/robyn.pyi
@@ -45,7 +45,7 @@ class Request:
 
     queries: dict[str, str]
     headers: dict[str, str]
-    params: dict[str, str]
+    path_params: dict[str, str]
     body: Union[str, bytes]
     method: str
     url: Url
