b0e71fa0741d0a118c586cae2c85e99b3d5cab32
Oluwaseun <tanimolaoluwaseun70@gmail.com>
2023-04-10 00:39:52 +0100

    feat: Robyn CLI (#466)
    * feat: cli
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * feat: cli improved
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * feat: cli improved
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * feat: cli improved
    
    * feat: cli improved
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ robyn/__main__.py
@@ -0,0 +1,28 @@
+import os
+import click
+
+
+@click.command()
+def create():
+    project_name = input("Enter the project name: ")
+    project_dir = input("Enter the name of the project directory: ")
+    # Initailize a new Robyn project
+
+    print(f"Creating a new Robyn project '{project_name}' in '{project_dir}'...")
+
+    # Create a new directory for the project
+    project_path = os.path.join(project_dir, project_name)
+    os.makedirs(project_path)
+
+    # Create the main application file
+    app_file_path = os.path.join(project_path, "app.py")
+    with open(app_file_path, "w") as f:
+        f.write(
+            'from robyn import Robyn\n\napp = Robyn(__file__)\n\nif __name__ == "__main__":\n app.run()'
+        )
+
+    print(f"New Robyn project '{project_name}' created in '{project_dir}' ")
+
+
+if __name__ == "__main__":
+    create()
