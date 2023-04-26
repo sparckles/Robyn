3b612b10fb9217b75c865ea8e2b8035bb1f5a316
Oluwaseun241 <tanimolaoluwaseun70@gmail.com>
2023-04-26 19:54:00 +0100

    chore: Cli improvements

--- robyn/__main__.py
+++ robyn/__main__.py
@@ -7,7 +7,6 @@ from .argument_parser import Config
 def create():
     project_dir = input("Enter the name of the project directory: ")
     docker = input("Need Docker? (Y/N) ")
-    
     # Initailize a new Robyn project
     def check(value):
         while value not in ['Y', 'N']:
