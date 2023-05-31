51515117385392d56ab99c75a2400a95c2b2137f
Sanskar Jethi <sansyrox@gmail.com>
2023-05-31 20:50:33 +0100

    fix: integration tests

--- integration_tests/test_subrouter.py
+++ integration_tests/test_subrouter.py
@@ -1,19 +1,18 @@
 import pytest
 import requests
 
+from helpers.http_methods_helpers import generic_http_helper, head
+
 
 @pytest.mark.parametrize(
     "http_method_type",
     ["get", "post", "put", "delete", "patch", "options", "trace"],
 )
 def test_sub_router(http_method_type, session):
-    response = requests.request(http_method_type, "http://127.0.0.1:8080/sub_router/foo")
-    assert response.status_code == 200
+    response = generic_http_helper(http_method_type, "sub_router/foo")
     assert response.json() == {"message": "foo"}
 
 
-# head request
 def test_sub_router_head(session):
-    response = requests.head("http://127.0.0.1:8080/sub_router/foo")
-    assert response.status_code == 200
+    response = head("sub_router/foo")
     assert response.text == ""  # response body is expected to be empty
