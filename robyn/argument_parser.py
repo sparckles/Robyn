499d564f600c98eecf16de776b438aac015b7352
IdoKendo <41922392+IdoKendo@users.noreply.github.com>
2023-06-17 08:30:46 -0700

    feat: add --open flag to open browser on successful start of the server (#517)
    * feat: add --open flag to open browser on successful start of the server
    
    * fix: rename flag from --open to --open-browser
    
    * feat: add ROBYN_BROWSER_OPEN environment variable as a way to open browser
    
    * add --open-browser explanation in README.md file

--- robyn/argument_parser.py
+++ robyn/argument_parser.py
@@ -45,6 +45,12 @@ class Config:
             default=False,
             help="Open the Robyn documentation.",
         )
+        parser.add_argument(
+            "--open-browser",
+            action="store_true",
+            default=False,
+            help="Open the browser on successful start.",
+        )
 
         args, _ = parser.parse_known_args()
 
@@ -53,6 +59,7 @@ class Config:
         self.dev = args.dev
         self.create = args.create
         self.docs = args.docs
+        self.open_browser = args.open_browser
 
         if self.dev and (self.processes != 1 or self.workers != 1):
             raise Exception("--processes and --workers shouldn't be used with --dev")
