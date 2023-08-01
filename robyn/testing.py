bf39918b4b62ed88f09cad0d66c3f6e3cd48c6ba
Electroely <131962959+ElyasMag@users.noreply.github.com>
2023-08-01 03:50:30 -0400

    Added first version of GET test method

--- robyn/testing.py
+++ robyn/testing.py
@@ -7,7 +7,26 @@
 # it's important to understand the following classes, at least right now:
 # Route, Request, Response
 
+import asyncio
+from robyn import Request, Response
+
+def get_route(app, path):
+	routes = app.router.get_routes()
+	r = None
+	for route in routes:
+		if route.route == path:
+			r = route
+			break
+	return r
+
 class TestingClient:
 	def __init__(self, app):
 		self.app = app
+	
+	def get(self, method_path, arg = None):
+		route = get_route(self.app, method_path)
+		if route == None:
+			return None
+		return asyncio.run(route.function.handler(None))
+		
 
