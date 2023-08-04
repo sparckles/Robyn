29d9f6e1f5fadc062c1a4244f1237e68c239c505
Kanishk Pachauri <itskanishkp.py@gmail.com>
2023-08-05 05:03:19 +0530

    fixed the doc's link

--- robyn/__main__.py
+++ robyn/__main__.py
@@ -79,7 +79,7 @@ CMD ["python3.10", "/workspace/foo/app.py", "--log-level=DEBUG"]
 
 def docs():
     print("Opening Robyn documentation... | Offline docs coming soon!")
-    webbrowser.open("https://robyn.tech/")
+    webbrowser.open("https://sparckles.github.io/robyn/#/")
 
 
 def version():
