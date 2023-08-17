9f9c48b162fab75b988dfb1f25b4adb7fb91b129
Electroely <131962959+ElyasMag@users.noreply.github.com>
2023-08-17 03:11:00 -0400

    Updated example files

--- testclient_example/testclient.py
+++ testclient_example/testclient.py
@@ -1,10 +1,18 @@
-from robyn.testing import TestClient
-from myapp import app
+from testing import TestClient
+from robyn_server import app
 
 client = TestClient(app)
 
 def test_index():
     response = client.get("/")
-    print(response)
+    assert response.status_code == 200
+    assert response.text == "Hello, Robyn!"
+
+def test_upload():
+    file_data = b"This is a test file"
+    response = client.post("/upload", files={"file": ("test_file.txt", file_data)})
+    assert response.status_code == 200
+    assert response.json() == {"message": "File uploaded successfully"}
 
 test_index()
+test_upload()
