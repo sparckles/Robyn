1f29cd03d55cbf714abf29d655e277103f204be1
AntoineRR <ant.romero2@orange.fr>
2024-02-04 21:00:16 +0100

    fix: tests for macos

--- integration_tests/helpers/network_helpers.py
+++ integration_tests/helpers/network_helpers.py
@@ -3,11 +3,10 @@ import platform
 
 
 def get_network_host():
-    hostname = socket.gethostname()
-    ip_address = socket.gethostbyname(hostname)
-
     # windows doesn't support 0.0.0.0
     if platform.system() == "Windows":
+        hostname = socket.gethostname()
+        ip_address = socket.gethostbyname(hostname)
         return ip_address
     else:
         return "0.0.0.0"
