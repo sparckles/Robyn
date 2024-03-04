812817f855541431cb8d69eb71d4ec78a3e8f1e9
carrycooldude <rawatkari554@gmail.com>
2024-03-04 15:28:13 +0530

    changed the test_get_request

--- integration_tests/test_get_requests.py
+++ integration_tests/test_get_requests.py
@@ -47,7 +47,7 @@ def test_template(function_type: str, session):
 def test_url_for(function_type: str, session):
     def check_response(r: Response):
         assert r.status_code == 200
-        assert r.text == f"/{function_type}/url_for"
+        assert r.text == f"<a href='/{function_type}/url_for'>/{function_type}/url_for</a>"
         assert "<!DOCTYPE html>" in r.text
         assert "Jinja2" in r.text
         assert "Robyn" in r.text
