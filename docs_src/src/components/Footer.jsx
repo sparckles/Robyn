fb02e9713f8e91cbdea65cfe9114a4a1955c69a1
Sanskar Jethi <sansyrox@gmail.com>
2024-07-15 14:17:27 +0100

    docs: fix styling of docs website

--- docs_src/src/components/Footer.jsx
+++ docs_src/src/components/Footer.jsx
@@ -26,8 +26,11 @@ export function GithubButton() {
 export function Footer() {
   return (
     <>
-      <Container className="bottom-2 w-[90%] z-40" style={{ position: 'fixed' }}>
-        <div className="flex justify-end md:flex-1 ]">
+      <Container
+        className="bottom-2 right-2 z-40"
+        style={{ position: 'fixed' }}
+      >
+        <div className="flex md:flex-1">
           <div className="pointer-events-auto ">
             <Link target="_blank" href="https://github.com/sparckles/robyn">
               <GithubButton />
