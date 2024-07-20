eb304105c36d376b3e77b9d12417ae056abf301f
Rahmad Afandi <rahmadafandiii@gmail.com>
2024-07-20 19:34:38 +0700

    feat: add support to multiple authentication

--- robyn/authentication.py
+++ robyn/authentication.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
-from typing import Optional
+from typing import Optional, Tuple, Union
+from base64 import b64decode
 
 from robyn.robyn import Headers, Identity, Request, Response
 from robyn.status_codes import HTTP_401_UNAUTHORIZED
@@ -45,15 +46,30 @@ class TokenGetter(ABC):
         """
         raise NotImplementedError()
 
+    @classmethod
+    def get_credentials(cls, request: Request):
+        """
+        Only available for Basic Getter.
+        Gets credentials from the request token basic.
+        This method will decode the token and return username password
+        :param request: The request object.
+        :return: Tuple of username and password.
+        """
+        pass
+
 
 class AuthenticationHandler(ABC):
-    def __init__(self, token_getter: TokenGetter):
+    def __init__(self, token_getter: TokenGetter, name: str, default: bool = False):
         """
         Creates a new instance of the AuthenticationHandler class.
         This class is an abstract class used to authenticate a user.
         :param token_getter: The token getter used to get the token from the request.
+        :param name: The name of authentication handler ex: jwt, basic, etc.
+        :param default: set authentication handler is the default handler.
         """
         self.token_getter = token_getter
+        self.name = name
+        self.default = default
 
     @property
     def unauthorized_response(self) -> Response:
@@ -94,3 +110,32 @@ class BearerGetter(TokenGetter):
     @classmethod
     def set_token(cls, request: Request, token: str):
         request.headers["Authorization"] = f"Bearer {token}"
+
+
+class BasicGetter(TokenGetter):
+    """
+    This class is used to get the token from the Authorization header.
+    The scheme of the header must be Basic.
+    """
+
+    @classmethod
+    def get_token(cls, request: Request) -> Optional[str]:
+        authorization_header = request.headers.get("authorization")
+        if not authorization_header or not authorization_header.startswith("Basic "):
+            return None
+
+        return authorization_header[6:]  # Remove the "Basic " prefix
+
+    @classmethod
+    def set_token(cls, request: Request, token: str):
+        request.headers["Authorization"] = f"Basic {token}"
+
+    @classmethod
+    def get_credentials(cls, request: Request) -> Union[Tuple[str, str], Tuple[None, None]]:
+        basic_token = cls.get_token(request)
+        try:
+            basic_token_decoded = b64decode(basic_token).decode()
+            username, _, password = basic_token_decoded.partition(":")
+            return (username, password)  # Return username password from basic authorization
+        except Exception:
+            return (None, None)
