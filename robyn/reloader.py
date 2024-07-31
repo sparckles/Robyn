8dd05e7bea57863ad27088722773ebe608d810d1
Kade-Powell <47295838+Kade-Powell@users.noreply.github.com>
2024-07-31 18:09:07 -0400

    fix: :bug: Fix bug in compile_rust_files where it returns a list of t… (#873)
    * fix: :bug: Fix bug in compile_rust_files where it returns a list of the rust source code files instead of binaries causing the source files to be deleted in the Event handler during reload
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: Powell, Kade (CCI-Southeast) <Kade.Powell@cox.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- robyn/reloader.py
+++ robyn/reloader.py
@@ -16,8 +16,10 @@ dir_path = None
 
 def compile_rust_files(directory_path: str):
     rust_files = glob.glob(os.path.join(directory_path, "**/*.rs"), recursive=True)
+    rust_binaries: list[str] = []
+
     for rust_file in rust_files:
-        print("Compiling rust file : %s", rust_file)
+        print(f"Compiling rust file: {rust_file}")
 
         result = subprocess.run(
             [sys.executable, "-m", "rustimport", "build", rust_file],
@@ -26,11 +28,27 @@ def compile_rust_files(directory_path: str):
             start_new_session=False,
         )
         if result.returncode != 0:
-            print("Error compiling rust file : %s %s", result.stderr.decode("utf-8"), result.stdout.decode("utf-8"))
+            print(f"Error compiling rust file: {rust_file} \n {result.stderr.decode('utf-8')} \n {result.stdout.decode('utf-8')}")
         else:
-            print("Compiled rust file : %s", rust_file)
+            print(f"Compiled rust file: {rust_file}")
+            rust_file_base = rust_file.removesuffix(".rs")
 
-    return rust_files
+            # Define the search pattern for the binary file
+            if sys.platform == "win32":
+                binary_extension = ".dll"
+            elif sys.platform == "darwin":
+                binary_extension = ".so"
+            elif sys.platform == "linux":
+                binary_extension = ".so"
+            else:
+                raise ValueError(f"Unsupported platform: {sys.platform}")
+
+            search_pattern = f"{rust_file_base}.*{binary_extension}"
+            # Use glob to find matching binary files
+            matching_binaries = glob.glob(search_pattern)
+            rust_binaries.extend(matching_binaries)
+
+    return rust_binaries
 
 
 def create_rust_file(file_name: str):
@@ -47,7 +65,11 @@ def create_rust_file(file_name: str):
     )
 
     if result.returncode != 0:
-        print("Error creating rust file : %s %s", result.stderr.decode("utf-8"), result.stdout.decode("utf-8"))
+        print(
+            "Error creating rust file : %s %s",
+            result.stderr.decode("utf-8"),
+            result.stdout.decode("utf-8"),
+        )
     else:
         print("Created rust file : %s", rust_file)
 
