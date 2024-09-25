10d3c8310567e7e63eb92f7edf371d17968db193
ashupednekar <ashutoshpednekar15@gmail.com>
2024-09-25 22:03:07 +0530

    chore: updated server

--- robyn/scaffold/structured/no-db/server.py
+++ robyn/scaffold/structured/no-db/server.py
@@ -1,10 +1,7 @@
 from robyn import Robyn
 from robyn.helpers import discover_routes
-from router import build_routes
 
-# app: Robyn = build_routes()
-# OR
-app: Robyn = discover_routes()
+app: Robyn = discover_routes("api.handlers")
 
 if __name__ == "__main__":
     app.start(host="0.0.0.0", port=8080)
