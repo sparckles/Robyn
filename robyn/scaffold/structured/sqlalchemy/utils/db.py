c0c5dd46ef562f391398c5f47e65997e88116f75
ashupednekar <ashutoshpednekar15@gmail.com>
2024-09-25 22:50:32 +0530

    chore: added db connection pool and envs to sqlalchemy structured scaffolding

---
+++ robyn/scaffold/structured/sqlalchemy/utils/db.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from conf import settings
+
+import logging
+logger = logging.getLogger(__name__)
+
+def get_pool():
+    dsn = settings.database_url
+    logger.info(f"creating client db pool with dsn: {dsn}")
+    engine = create_engine(
+        dsn,
+        pool_size=settings.db_pool_size,
+        max_overflow=settings.db_pool_max_overflow,
+        pool_timeout=settings.db_pool_timeout,
+        pool_recycle=settings.db_pool_recycle,
+        echo=settings.db_pool_recycle
+    )
+    return sessionmaker(bind=engine)
