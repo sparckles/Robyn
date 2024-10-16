128942149cb8958c426f0be58c40dfb1fbf3ee4b
ashupednekar <ashutoshpednekar15@gmail.com>
2024-10-16 14:26:35 +0530

    chore: removed class groups on handlers in favour of straigtforward handler functions

--- robyn/scaffold/structured/no-db/api/handlers/sample.py
+++ robyn/scaffold/structured/no-db/api/handlers/sample.py
@@ -3,18 +3,12 @@ from robyn import SubRouter
 router = SubRouter(__name__, "/sample")
 
 
-class SampleHandlers:
-    """
-    note: the handles being grouped in a class like this is complete optional, and doesn't have any impact on routing
-    """
+@router.post("/one")
+def one(): ...
 
-    @router.post("/one")
-    @staticmethod
-    def one(): ...
 
-    @router.get("/two")
-    @staticmethod
-    def two(): ...
+@router.get("/two")
+def two(): ...
 
 
 @router.get("three/")
--- robyn/scaffold/structured/sqlalchemy/api/handlers/sample.py
+++ robyn/scaffold/structured/sqlalchemy/api/handlers/sample.py
@@ -6,25 +6,19 @@ from robyn import SubRouter
 router = SubRouter(__name__, prefix="/sample/")
 
 
-class SampleHandlers:
-    """
-    note: the handles being grouped in a class like this is complete optional, and doesn't have any impact on routing
-    """
+@router.post("one/")
+async def one(global_dependencies):
+    pool = global_dependencies.get("pool")
+    async with get_db_connection(pool) as conn:
+        # invoke your mutators/selectors here
+        res = await sample_selector(conn)
+        print(res)
+    return {}
 
-    @staticmethod
-    @router.post("one/")
-    async def one(global_dependencies):
-        pool = global_dependencies.get("pool")
-        async with get_db_connection(pool) as conn:
-            # invoke your mutators/selectors here
-            res = await sample_selector(conn)
-            print(res)
-        return {}
 
-    @staticmethod
-    @router.get("two/")
-    def two():
-        return {}
+@router.get("two/")
+def two():
+    return {}
 
 
 @router.get("three/")
