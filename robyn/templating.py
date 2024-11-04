032c247512b75cffdcb3813a54ef1f0daa7ac571
Dave Warnock <dwarnock@gmail.com>
2024-11-04 16:26:22 +0000

    Added abstract methodist to interface, added a docstring for set_robyn. ruff fixes. Safety check for no robyn object.

--- robyn/templating.py
+++ robyn/templating.py
@@ -15,12 +15,18 @@ class TemplateInterface(ABC):
     @abstractmethod
     def render_template(self, *args, **kwargs) -> Response: ...
 
+    @abstractmethod
+    def set_robyn(self, robyn: Robyn) -> None: ...
+
+    @abstractmethod
+    def url_for(self, function_name: str, route_type: HttpMethod = HttpMethod.GET) -> str: ...
+
 
 class JinjaTemplate(TemplateInterface):
-    def __init__(self, directory, encoding="utf-8", followlinks=False):
+    def __init__(self, directory, encoding="utf-8", followlinks=False) -> None:
         self.env: Environment = Environment(loader=FileSystemLoader(searchpath=directory, encoding=encoding, followlinks=followlinks))
         self.add_function_to_globals("url_for", self.url_for)
-        self.robyn = None
+        self.robyn: Robyn | None = None
 
     def add_function_to_globals(self, name: str, func: Callable):
         """
@@ -29,7 +35,13 @@ class JinjaTemplate(TemplateInterface):
         self.env.globals[name] = func
 
     def set_robyn(self, robyn: Robyn) -> None:
-        self.robyn: Robyn = robyn
+        """
+        The url_for needs to have access to the list of routes stored in the apps Robyn object
+
+        Args:
+            robyn (Robyn): The top instance of the Robyn class for this app.
+        """
+        self.robyn = robyn
 
     def url_for(self, function_name: str, route_type: HttpMethod = HttpMethod.GET) -> str:
         """Creates a link to an endpoint function name
@@ -38,13 +50,16 @@ class JinjaTemplate(TemplateInterface):
             str: the url for the function
         """
 
+        if self.robyn is None:
+            return "url_for needs set_robyn"
+
         routes: List[Route] = self.robyn.router.get_routes()
 
         for r in routes:
             if r.function.handler.__name__ == function_name and r.route_type == route_type:
                 return r.route
 
-        return "route not found"
+        return "route not found in Robyn router"
 
     def render_template(self, template_name, **kwargs) -> Response:
         rendered_template = self.env.get_template(template_name).render(**kwargs)
