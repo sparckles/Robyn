b04fec7ffc5cbef7300a3d3adb000fc8d905cc41
Dave Warnock <dwarnock@gmail.com>
2024-11-11 15:37:26 +0000

    failing tests for subrouter authentication: AuthenticationNotConfiguredError: Authentication is not configured. Use app.configure_authentication() to configure it.

--- integration_tests/subroutes/di_subrouter.py
+++ integration_tests/subroutes/di_subrouter.py
@@ -15,3 +15,20 @@ def sync_subrouter_route_dependency(r: Request, router_dependencies, global_depe
 @di_subrouter.get("/subrouter_global_di")
 def sync_subrouter_global_dependency(global_dependencies):
     return global_dependencies["GLOBAL_DEPENDENCY"]
+
+
+# ===== Authentication =====
+
+
+@di_subrouter.get("/subrouter_router_di/sync/auth", auth_required=True)
+def sync_subrouter_auth(request: Request):
+    assert request.identity is not None
+    assert request.identity.claims == {"key": "value"}
+    return "authenticated"
+
+
+@di_subrouter.get("/subrouter_router_di/async/auth", auth_required=True)
+async def async_subrouter_auth(request: Request):
+    assert request.identity is not None
+    assert request.identity.claims == {"key": "value"}
+    return "authenticated"
