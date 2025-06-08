c352705743fecdee82aa9091ba4503dae9ad45ba
Vishnu Sanal T <t.v.s10123@gmail.com>
2025-06-08 19:45:40 +0530

    fix: maturin not found

--- noxfile.py
+++ noxfile.py
@@ -6,6 +6,7 @@ import nox
 @nox.session(python=["3.9", "3.10", "3.11", "3.12", "3.13"])
 def tests(session):
     session.run("pip", "install", "poetry==1.3.0")
+    session.run("pip", "install", "maturin")
     session.run(
         "poetry",
         "export",
