b6b1b25ad7a0a984e27a1b0d33e667d63da107fe
Ritwik Tiwari <ritwiktiwari24@gmail.com>
2026-02-21 22:09:43 -0800

    feat: address comments

--- noxfile.py
+++ noxfile.py
@@ -10,6 +10,7 @@ def tests(session):
     session.run(
         "uv",
         "export",
+        "--frozen",
         "--group",
         "test",
         "--group",
@@ -45,4 +46,4 @@ def tests(session):
 def lint(session):
     session.run("pip", "install", "ruff")
     session.run("ruff", "check", ".")
-    session.run("ruff", "format", ".")
+    session.run("ruff", "format", "--check", ".")
