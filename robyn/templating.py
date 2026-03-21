71ad5726b24ae518e782b015ed2ec494d709335e
Rin <dangnhatrin90@gmail.com>
2026-03-22 05:12:28 +0700

    fix: enable default autoescape for Jinja2 templates (#1321)
    * security: enable Jinja2 autoescape by default to prevent XSS
    
    * Add missing docstrings to satisfy the coverage threshold in templating.py

--- robyn/templating.py
+++ robyn/templating.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 
-from jinja2 import Environment, FileSystemLoader
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 from robyn import status_codes
 
@@ -8,17 +8,61 @@ from .robyn import Headers, Response
 
 
 class TemplateInterface(ABC):
-    def __init__(self): ...
+    """
+    Interface for implementing various template engines in Robyn.
+    """
+
+    def __init__(self):
+        """
+        Initializes the template interface.
+        """
+        ...
 
     @abstractmethod
-    def render_template(self, *args, **kwargs) -> Response: ...
+    def render_template(self, *args, **kwargs) -> Response:
+        """
+        Renders a template with the given arguments.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            Response: The rendered template response.
+        """
+        ...
 
 
 class JinjaTemplate(TemplateInterface):
+    """
+    Jinja2 implementation of the TemplateInterface.
+    """
+
     def __init__(self, directory, encoding="utf-8", followlinks=False):
-        self.env = Environment(loader=FileSystemLoader(searchpath=directory, encoding=encoding, followlinks=followlinks))
+        """
+        Initializes the Jinja2 template environment with autoescape enabled for HTML and XML files.
+
+        Args:
+            directory (str): The directory where templates are located.
+            encoding (str): The encoding to use when loading templates.
+            followlinks (bool): Whether to follow symbolic links in the template directory.
+        """
+        self.env = Environment(
+            loader=FileSystemLoader(searchpath=directory, encoding=encoding, followlinks=followlinks),
+            autoescape=select_autoescape(enabled_extensions=("html", "htm", "xml"), default=True),
+        )
 
     def render_template(self, template_name, **kwargs) -> Response:
+        """
+        Renders a Jinja2 template.
+
+        Args:
+            template_name (str): The name of the template file to render.
+            **kwargs: Variables to pass to the template.
+
+        Returns:
+            Response: The rendered template as a Robyn Response object.
+        """
         rendered_template = self.env.get_template(template_name).render(**kwargs)
         return Response(
             status_code=status_codes.HTTP_200_OK,
